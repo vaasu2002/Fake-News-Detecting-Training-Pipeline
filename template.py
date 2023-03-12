ffdb1eef8845526a7d767dc7154640f528026af5
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-03-12 09:30:36 +0000

    template file

---
+++ template.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format= "[%(asctime)s: %(levelname)s]: %(message)s"
+    )
+
+while True:
+    project_name = input("Enter the Project Name: ")
+    if project_name != '':
+        break
+
+logging.info(f"Creating project by name: {project_name}")
+
+# list of files:
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/cloud_storage/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py/__init__.py",
+    f"{project_name}/components/data_validation.py/__init__.py",
+    f"{project_name}/components/data_transformation.py/__init__.py",
+    f"{project_name}/components/model_trainer.py/__init__.py",
+    f"{project_name}/components/model_pusher.py/__init__.py",
+    f"{project_name}/components/model_evaluation.py/__init__.py",
+
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/data_access/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/ml/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/prediction.py",
+    f"{project_name}/pipeline/training.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"requirements.txt",
+    f"setup.py",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating a directory at: {filedir} for file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating a new file: {filename} at path: {filepath}")
+    else:
+        logging.info(f"file is already present at: {filepath}")
