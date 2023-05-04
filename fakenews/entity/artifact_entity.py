92b148bcf10328485cc45ca2e6d316bdcdcd6fbb
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-05-04 09:56:26 +0530

    DataTransformationArtifact added

--- fakenews/entity/artifact_entity.py
+++ fakenews/entity/artifact_entity.py
@@ -4,3 +4,9 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path: str
     test_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
