a06ad4026a4d20be40931a69e141900b9628eb23
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-05-04 09:56:37 +0530

    DataTransformationConfig added

--- fakenews/entity/config_entity.py
+++ fakenews/entity/config_entity.py
@@ -34,3 +34,21 @@ class DataIngestionConfig:
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
 
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+
+        # artifact/DATE_TIME/data_transformation
+        self.data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+
+        # artifact/DATE_TIME/data_transformation/transformed/train
+        self.transformed_train_file_path: str = os.path.join(self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+
+        # artifact/DATE_TIME/data_transformation/transformed/test
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        
+        # artifact/DATE_TIME/data_transformation/transformed_object
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCSSING_OBJECT_FILE_NAME,)
